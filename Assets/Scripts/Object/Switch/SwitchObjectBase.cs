7017274d1bc7ed4786d99bf3c260ff37aa7af513
bj961 <73685098+bj961@users.noreply.github.com>
2024-06-10 23:31:31 +0900

    주석 정리

--- Assets/Scripts/Object/Switch/SwitchObjectBase.cs
+++ Assets/Scripts/Object/Switch/SwitchObjectBase.cs
@@ -7,8 +7,6 @@ using UnityEngine.UIElements;
 
 public abstract class SwitchObjectBase : MonoBehaviour
 {
-    //public Transform targetObject;
-    //protected ITriggerable target;
     [SerializeField] protected bool isSwitchOn;
 
     protected void Start()
